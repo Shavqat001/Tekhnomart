aef1d5e12e107126c6bb7f589a2a0ede4cd79d8b
Shavqqat001 <nazriev.shavqatjon@gmail.com>
2023-03-25 15:34:29 +0500

    add js

---
+++ js/popup.js
@@ -0,0 +1,27 @@
+let popup = document.querySelector('.modal-show');
+let closeButton = document.querySelector('.modal-close');
+let showFormButton = document.querySelector('.contacts__link.red-button');
+let pictrueMap = document.querySelector('.contacts > img');
+let map = document.querySelector('.map-modal');
+let closeMapButton = document.querySelector('.close-map');
+pictrueMap.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    map.classList.remove('visually-hidden');
+});
+closeMapButton.addEventListener('click', function (evt) {
+    evt.preventDefault()
+    map.classList.add('visually-hidden');
+});
+showFormButton.addEventListener('click', function (evt) {
+    evt.preventDefault()
+    popup.classList.remove('visually-hidden');
+});
+
+window.addEventListener('load', function (evt) {
+    evt.preventDefault();
+
+});
+closeButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('visually-hidden');
+});
